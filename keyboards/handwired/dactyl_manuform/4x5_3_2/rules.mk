c147910a0ab9aa558c3434721537f6d5667d2694
ghaith <ghaith.sassi@gs.com>
2026-08-20 10:02:49 +0200

    fix split + rgb + keys

--- keyboards/handwired/dactyl_manuform/4x5_3_2/rules.mk
+++ keyboards/handwired/dactyl_manuform/4x5_3_2/rules.mk
@@ -1,5 +1,6 @@
-OLED_ENABLE = yes
+OLED_ENABLE = no
 OLED_TRANSPORT = i2c
 LTO_ENABLED = yes
 WPM_ENABLE = yes
-RGBLIGHT_ENABLE = yes
+# RGBLIGHT_ENABLE = yes
+RGB_MATRIX_ENABLE = yes       # Enable for pretty RGB matrix effects
